7885919e1b338123bc8db43dbdd6b195cfac6172
AUsatenko <andrey.m.usatenko@gmail.com>
2020-06-14 21:49:36 +0200

    Override getLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,30 @@ namespace GradeBook.GradeBooks
     public RankedGradeBook(string name) : base(name)
     {
       Type = GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      if(Students.Count < 5) throw new InvalidOperationException();
+
+      var averageGradeList = Students.Select(i => i.AverageGrade).ToList();
+      averageGradeList.Add(averageGrade);
+      var averageGradeArray = averageGradeList.OrderBy(i => i).ToArray();
+      var index = Array.IndexOf(averageGradeArray,averageGrade);
+      var step = averageGradeArray.Length / 5;
+
+      if(index >=0 && index < step){
+        return 'A';
+      }else if(index <= step*2){
+        return 'B';
+      }else if(index <= step*3){
+        return 'C';
+      }else if(index <= step*4){
+        return 'D';
+      }else if(index <= step*5){
+        return 'E';
+      }
+      return 'F';
     }
   }
 }
